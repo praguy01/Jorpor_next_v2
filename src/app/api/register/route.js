4800cfa2b679c5fb6769fbb1ebde8a5ad244ac2a
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-07 19:17:25 +0700

    deploy2

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -77,8 +77,8 @@ export async function POST(request)  {
       const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
+          user: 'platformsjorpor@gmail.com',
+          pass: 'qnzhidjxikwyiocl',
         },
       });
 
@@ -86,7 +86,7 @@ export async function POST(request)  {
       const confirmationCode = Math.floor(100000 + Math.random() * 900000);
 
       const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: 'platformsjorpor@gmail.com',
         to: data.email,
         subject: 'ยืนยันการเปลี่ยนรหัสผ่าน',
         text: `รหัสยืนยัน: ${confirmationCode}`,
