7e6b6014c8df7900f24b8fa927f9067ec8dd3ee4
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-09 13:04:01 +0700

    deploy5

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql2");
+require('dotenv').config();
+
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "nextjs-jorpor"
-}).promise()
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE || "nextjs-jorpor"
+}).promise();
 
 module.exports = db;
