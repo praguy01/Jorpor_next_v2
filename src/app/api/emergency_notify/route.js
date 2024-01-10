922eec655a400237f6d7ea761fdabeaa3fed30ea
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-10 11:11:51 +0700

    deploy16

--- src/app/api/emergency_notify/route.js
+++ src/app/api/emergency_notify/route.js
@@ -1,8 +1,10 @@
 // File: D:/SeniorNextjs/jorpor-nextjs/src/app/api/emergency_notify/route.ts
+import db from '../../../lib/db';
+import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   // Check if the request method is POST
-  if (request.method === 'POST') {
+  if (request.method === 'POST' && request.headers.get('Content-Type') === 'application/json') {
     try {
       // Parse the JSON data from the request
       const res = await request.json();
@@ -10,17 +12,26 @@ export async function POST(request) {
       // Log the received data
       console.log('Received data from NodeMCU:', res);
 
+      // Access query parameters
+      const { date, time, location } = res;
+
+      // Do something with the query parameters (e.g., log them)
+      console.log(' - Date:', date, 'Time:', time, 'Location:', location);
+
+      const insertSql = "INSERT INTO emergency_notify (date, time, location) VALUES (?, ?, ?)";
+      const insertValues = await db.execute(insertSql , [res.date, res.time ,res.location]);
+
       // Return a JSON response indicating success
-      return new NextResponse.json({ success: true, message: 'Successfully received data' });
+      return NextResponse.json({ success: true, message: 'Successfully received data' });
     } catch (error) {
       // Handle errors during processing
       console.error('Error processing the request:', error);
 
       // Return a JSON response indicating failure
-      return new NextResponse.json({ success: false, error: 'Failed to process the request' });
+      return NextResponse.json({ success: false, error: 'Failed to process the request' });
     }
   } else {
-    // Return a JSON response for the case of an invalid request method
-    return new NextResponse.json('Method not allowed');
+    // Return a JSON response for the case of an invalid request method or Content-Type
+    return NextResponse.json('Method not allowed or invalid Content-Type');
   }
 }
