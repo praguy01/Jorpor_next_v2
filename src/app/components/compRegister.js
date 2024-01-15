9db5c4bf8b87068d17f784f737b617e3ccdb45bf
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-16 00:59:07 +0700

    deploy59

--- src/app/components/compRegister.js
+++ src/app/components/compRegister.js
@@ -119,12 +119,12 @@ export default function CompRegister() {
               
             
 
-          // setRegistrationMessage('');
-          // setRegistrationMessagePass("Already registered.");
-          // setTimeout(() => {
-          //   setIsLoading(true); // เริ่มแสดง Loading
-          //   window.location.href = resdata.redirect;
-          // }, 1000); // รอ 1 วินาทีก่อนเปลี่ยนหน้า
+          setRegistrationMessage('');
+          setRegistrationMessagePass("Already registered.");
+          setTimeout(() => {
+            setIsLoading(true); // เริ่มแสดง Loading
+            window.location.href = resdata.redirect;
+          }, 1000); // รอ 1 วินาทีก่อนเปลี่ยนหน้า
         } else {
           setRegistrationMessage(resdata.error);
           setMessage('');
