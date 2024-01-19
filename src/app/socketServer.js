62372cef25892606b6d27799edc4f3df89325d01
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-20 00:13:38 +0700

    deploy72

--- src/app/socketServer.js
+++ src/app/socketServer.js
@@ -6,7 +6,7 @@ const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'wss://platform-jorpor.up.railway.app',
+    origin: 'wss://platform-jorpor-chada.koyeb.app',
     methods: ['GET', 'POST'],
     // credentials: true,
   },
