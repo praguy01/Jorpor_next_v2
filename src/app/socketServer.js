03918776a4100d3b8b266607822b238765e9d6e6
Chadaphorn0402 <tangmo125x@gmail.com>
2024-01-20 15:07:03 +0700

    deploy75

--- src/app/socketServer.js
+++ src/app/socketServer.js
@@ -1,22 +1,52 @@
+// import http from 'http';
+// import { Server } from 'socket.io';
+// import cors from 'cors';
+
+// const httpServer = http.createServer();
+// console.log("88888888888888888888888888888888888888888888888888888")
+
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: 'https://platform-jorpor-chada.koyeb.app',
+//     methods: ['GET', 'POST'],
+//     // credentials: true,
+//   },
+//   transports: ['websocket'],
+//   path: '/socket.io',
+// });
+
+
+
+
+// io.on('connection', (socket) => {
+//   console.log('Socket.IO connected');
+// });
+
+// io.on('error', (error) => {
+//   console.error('Socket.IO Error:', error);
+// });
+
+
+// const PORT = 8000; 
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Socket.IO server running on http://localhost:${PORT}`);
+// });
+
+
+// export { io };
+
 import http from 'http';
 import { Server } from 'socket.io';
-import cors from 'cors';
 
 const httpServer = http.createServer();
 console.log("88888888888888888888888888888888888888888888888888888")
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: 'https://platform-jorpor-chada.koyeb.app',
-    methods: ['GET', 'POST'],
-    // credentials: true,
-  },
+  // ไม่มีการกำหนดค่า CORS ในที่นี้
   transports: ['websocket'],
   path: '/socket.io',
 });
-
-
-
 
 io.on('connection', (socket) => {
   console.log('Socket.IO connected');
@@ -26,12 +56,10 @@ io.on('error', (error) => {
   console.error('Socket.IO Error:', error);
 });
 
-
-const PORT = 4001; 
+const PORT = 8000; 
 
 httpServer.listen(PORT, () => {
   console.log(`Socket.IO server running on http://localhost:${PORT}`);
 });
-
 
 export { io };
